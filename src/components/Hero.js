0173d6b23ec10ad848e0fc416b6e64d1daadd8c2
Trey Jackson <treyjackson@TreyJacksonMBP.lan>
2022-10-11 19:49:19 -0500

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import logo from "../assets/sanders-hyland-logo-lg.png";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <h1 className="mb-4">Dashboard</h1>
+
+    <p className="lead">
+
+    </p>
+  </div>
+);
+
+export default Hero;
